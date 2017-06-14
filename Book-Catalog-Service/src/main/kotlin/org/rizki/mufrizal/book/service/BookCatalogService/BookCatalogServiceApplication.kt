e20d7e317f63ccdafb5eb0057970cd8224b16907
RizkiMufrizal <mufrizalrizki@gmail.com>
2017-06-14 10:26:20 +0700

    intial project book catalog service

---
+++ Book-Catalog-Service/src/main/kotlin/org/rizki/mufrizal/book/service/BookCatalogService/BookCatalogServiceApplication.kt
@@ -0,0 +1,11 @@
+package org.rizki.mufrizal.book.service.BookCatalogService
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class BookCatalogServiceApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(BookCatalogServiceApplication::class.java, *args)
+}
