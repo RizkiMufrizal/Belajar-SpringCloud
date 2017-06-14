3dcb8c4dd91a395279a7b2742817262d2d7204d7
RizkiMufrizal <mufrizalrizki@gmail.com>
2017-06-14 11:52:35 +0700

    tambah interface validation helper

---
+++ Book-Catalog-Service/src/main/kotlin/org/rizki/mufrizal/book/service/BookCatalogService/helpers/ValidationIdHelper.kt
@@ -0,0 +1,8 @@
+package org.rizki.mufrizal.book.service.BookCatalogService.helpers
+
+/**
+ * Created by rizkimufrizal on 6/14/17.
+ */
+interface ValidationIdHelper {
+    fun validateSelf(id: Long)
+}
