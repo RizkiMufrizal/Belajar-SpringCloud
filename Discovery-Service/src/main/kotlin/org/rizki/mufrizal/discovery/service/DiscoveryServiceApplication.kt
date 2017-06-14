54ce1132c19e0aaf4ce625974ea1a36eade083dd
RizkiMufrizal <mufrizalrizki@gmail.com>
2017-06-14 09:30:19 +0700

    tambah enable eureka server

--- Discovery-Service/src/main/kotlin/org/rizki/mufrizal/discovery/service/DiscoveryServiceApplication.kt
+++ Discovery-Service/src/main/kotlin/org/rizki/mufrizal/discovery/service/DiscoveryServiceApplication.kt
@@ -2,8 +2,10 @@ package org.rizki.mufrizal.discovery.service
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
 @SpringBootApplication
+@EnableEurekaServer
 class DiscoveryServiceApplication
 
 fun main(args: Array<String>) {
