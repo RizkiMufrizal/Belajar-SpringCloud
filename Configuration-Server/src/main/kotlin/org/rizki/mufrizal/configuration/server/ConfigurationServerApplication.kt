acfc377846bb75215daff7c5df928e2b3cec3d83
RizkiMufrizal <mufrizalrizki@gmail.com>
2017-06-14 09:47:14 +0700

    tambah enable config server dan discovery client

--- Configuration-Server/src/main/kotlin/org/rizki/mufrizal/configuration/server/ConfigurationServerApplication.kt
+++ Configuration-Server/src/main/kotlin/org/rizki/mufrizal/configuration/server/ConfigurationServerApplication.kt
@@ -2,8 +2,12 @@ package org.rizki.mufrizal.configuration.server
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.config.server.EnableConfigServer
 
 @SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
 class ConfigurationServerApplication
 
 fun main(args: Array<String>) {
