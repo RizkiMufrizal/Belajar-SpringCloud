35fea738e303bb1af6e53ea15a1666e6d9b5beb6
RizkiMufrizal <mufrizalrizki@gmail.com>
2017-06-14 11:52:49 +0700

    tambah enable discovery client

--- Book-Catalog-Service/src/main/kotlin/org/rizki/mufrizal/book/service/BookCatalogService/BookCatalogServiceApplication.kt
+++ Book-Catalog-Service/src/main/kotlin/org/rizki/mufrizal/book/service/BookCatalogService/BookCatalogServiceApplication.kt
@@ -2,8 +2,10 @@ package org.rizki.mufrizal.book.service.BookCatalogService
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @SpringBootApplication
+@EnableDiscoveryClient
 class BookCatalogServiceApplication
 
 fun main(args: Array<String>) {
