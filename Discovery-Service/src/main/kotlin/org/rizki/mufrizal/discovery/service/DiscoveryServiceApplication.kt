3313405326b1ac8fb60fd7df9bffbd0f08fd7767
RizkiMufrizal <mufrizalrizki@gmail.com>
2017-06-14 09:09:36 +0700

    initial project discovery service

---
+++ Discovery-Service/src/main/kotlin/org/rizki/mufrizal/discovery/service/DiscoveryServiceApplication.kt
@@ -0,0 +1,11 @@
+package org.rizki.mufrizal.discovery.service
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class DiscoveryServiceApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(DiscoveryServiceApplication::class.java, *args)
+}
