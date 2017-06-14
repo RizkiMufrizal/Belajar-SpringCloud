2f35042aeb64816330d2e2ee12849fe762eee3d8
RizkiMufrizal <mufrizalrizki@gmail.com>
2017-06-14 13:25:19 +0700

    tambah migration contoh data book catalog

--- Book-Catalog-Service/src/main/kotlin/org/rizki/mufrizal/book/service/BookCatalogService/BookCatalogServiceApplication.kt
+++ Book-Catalog-Service/src/main/kotlin/org/rizki/mufrizal/book/service/BookCatalogService/BookCatalogServiceApplication.kt
@@ -1,12 +1,31 @@
 package org.rizki.mufrizal.book.service.BookCatalogService
 
+import org.rizki.mufrizal.book.service.BookCatalogService.domain.BookCatalog
+import org.rizki.mufrizal.book.service.BookCatalogService.repository.BookCatalogRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import java.math.BigDecimal
 
 @SpringBootApplication
 @EnableDiscoveryClient
-class BookCatalogServiceApplication
+class BookCatalogServiceApplication @Autowired constructor(val bookCatalogRepository: BookCatalogRepository) : CommandLineRunner {
+    override fun run(vararg args: String?) {
+        bookCatalogRepository.deleteAll()
+        for (i in 1..10) {
+            bookCatalogRepository.save(
+                    BookCatalog(
+                            bookName = "Buku ke $i",
+                            bookTotal = i,
+                            bookPrice = BigDecimal(i * 10),
+                            bookDescription = "buku ini merupakan buku dengan deskripsi $i"
+                    )
+            )
+        }
+    }
+}
 
 fun main(args: Array<String>) {
     SpringApplication.run(BookCatalogServiceApplication::class.java, *args)
